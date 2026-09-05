55483732293e174b369be765cffa4a26a476eedb
Shoontaro <tishinadw@gmail.com>
2026-09-05 19:31:22 +0300

    Добавьте файлы проекта.

---
+++ Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoListAPI.Models
+{
+    public class Todo
+    {
+        public int id { get; set; }
+        public string? title { get; set; }
+        public string? description { get; set; }
+    }
+}
